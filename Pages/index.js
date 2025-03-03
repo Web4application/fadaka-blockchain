33badc1c33a9b93e861599025da652aafe26bdc0
Web4 <167559384+Web4application@users.noreply.github.com>
2025-03-03 13:36:26 -0500

    Update and rename index.js to index.js
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- Pages/index.js
+++ Pages/index.js
@@ -3,7 +3,7 @@ import AIFunctions from '../components/AIFunctions';
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Web4asset</h1>
+      <h1>Welcome to fadaka</h1>
       <AIFunctions />
     </div>
   );
