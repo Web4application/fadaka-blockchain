7f3cafe13b22378935986db0df6041481dcafa5c
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-10 01:36:58 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -31,3 +31,8 @@ artifacts/contracts/MyContract.sol/MyContract.json
 npm init -y
 npm install --save-dev hardhat
 npx hardhat
+go install mvdan.cc/gofumpt@v0.4.0
+go install honnef.co/go/tools/cmd/staticcheck@v0.4.2
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.60.3
+go install go.uber.org/nilaway/cmd/nilaway@v0.0.0-20240821220108-c91e71c080b7
+go install github.com/daixiang0/gci@v0.11.2
