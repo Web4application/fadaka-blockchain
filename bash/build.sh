a35b816b0fa4c25e3b9c442b71b770dbb491ab60
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-10 07:32:39 +0200

    Update build.sh

--- bash/build.sh
+++ bash/build.sh
@@ -3,3 +3,11 @@ set -e
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen target/wasm32-unknown-unknown/release/fadaka_wasm.wasm \
   --out-dir pkg --target web
+# yarn
+yarn
+
+# npm
+npm install
+
+# pnpm
+pnpm install --shamefully-hoist
