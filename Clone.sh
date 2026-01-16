567a5b41a9dd7a191fc2a85bc73d8a1baa4f629d
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-16 13:49:38 +0100

    Create Clone.sh

---
+++ Clone.sh
@@ -0,0 +1,8 @@
+forge init eip-7702-project
+cd eip-7702-project
+
+git clone https://github.com/quiknode-labs/qn-guide-examples.git
+cd qn-guide-examples/ethereum/eip-7702
+
+forge install foundry-rs/forge-std
+forge install OpenZeppelin/openzeppelin-contracts
